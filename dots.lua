ca6fb8bb0af152fdf1af2561bac2663115854349
Jim Fowler <fowler@math.osu.edu>
2018-02-16 12:09:21 -0500

    Fixed sum

--- dots.lua
+++ dots.lua
@@ -6,72 +6,127 @@
 -- (circle(5) - 1) * (grid(2,2) - 1)
 -- also (grid(2,2) + 1) is permitted
 
-function compose(a,b)
+local M = {}
+local Card = {}
+
+local function compose(a,b)
    return Card.new(
-      {render=function(h)
-	  return a.render( function() b.render(h) end )
+      {render=function(self, h)
+	  return a:render( function() b:render(h) end )
       end
    })
 end
 
-function sum(f,g)
+local function skip(f, j)
+   if (type(j) ~= "number") then
+      j = j:count()
+   end   
+   
    return Card.new(
-      {render=function(h)
-	  grid(2,1).render( function(i)
-		if i == 1 then
-		   return f.render(h)
-		else
-		   return g.render(h)
+      {render=
+	  function(self, h)
+	     local i = 1
+	     return f:render(
+		function()
+		   if (i > j) then
+		      h()
+		   end
+		   i = i + 1
 		end
-			    end
-			  )
-      end
+	     )
+	  end
    })
 end
 
-function stack(f,g)
-   return Card.new(
-      {render=function(h)
-	  grid(1,2).render( function(i)
-		if i == 1 then
-		   return f.render(h)
-		else
-		   return g.render(h)
-		end
-			    end
-			  )
-      end
-   })
+function M.dot()
+   love.graphics.push()
+   local lineWidth = 10
+   love.graphics.setLineWidth( lineWidth )
+   love.graphics.circle( 'fill', 0, 0, 50 - lineWidth/2 )
+   love.graphics.circle( 'line', 0, 0, 50 - lineWidth/2 )   
+   love.graphics.pop()
 end
 
--- remove the first "count" things from this
-function skip(f, count)
-   return function(dot, i)
-      if (i > j) then
-	 dot(dot,i - j)
-      end
+function M.todots(n)
+   if     (n == 1) then return M.grid(1,1)
+   elseif (n == 2) then return M.grid(2,1)
+   elseif (n == 3) then return M.circle(3)
+   elseif (n == 4) then return M.grid(2,2)
+   elseif (n == 5) then return M.circle(5)
+   elseif (n == 6) then return M.grid(3,2)
+   elseif (n == 7) then return M.circle(7)
+   elseif (n == 8) then return M.grid(4,2)
+   elseif (n == 9) then return M.grid(3,3)
+   elseif (n == 10) then return M.grid(5,2)
+   else return M.circle(n)
    end
 end
 
-Card = {}
+local function sum(f,g)
+   if (type(g) == "number") then
+      g = M.todots(g)
+   end
+   
+   return Card.new(
+      {render=function(self, h)
+	  local i = 0
+	  M.grid(2,1):render(
+	     function()
+		i = i + 1		
+		if i == 1 then
+		   return f:render(h)
+		else
+		   return g:render(h)
+		end
+	     end
+			    )
+      end
+   })
+end
+
+local function stack(f,g)
+   return Card.new(
+      {render=function(self, h)
+	  local i = 0
+	  M.grid(1,2):render( function()
+		i = i + 1		
+		if i == 1 then
+		   return f:render(h)
+		else
+		   return g:render(h)
+		end
+			      end
+			  )
+      end
+   })
+end
+
 Card.prototype = {}
 Card.mt = {
    __mul = compose,
    __add = sum,
-   __div = stack,
+   __concat = stack,
    __sub = skip
 }
 
 function Card.new (o)
    setmetatable(o, Card.mt)
+   
+   o.count = function(self)
+      local i = 0
+      o:render( function()
+	    i = i + 1
+      end)
+      return i
+   end
+   
    return o
 end
 
-function grid(cx,cy)
+function M.grid(cx,cy)
    return Card.new(
-      {render=function(f)
+      {render=function(self, f)
 	  local count = math.max(cx,cy)
-	  local i = 1
 	  love.graphics.push()
 	  love.graphics.scale( 1 / math.sqrt(2) )
 	  for x = 0, cx - 1 do
@@ -82,8 +137,7 @@ function grid(cx,cy)
 		love.graphics.scale( 1 / count )
 		love.graphics.translate( 50, 50 )
 		love.graphics.scale( 0.9 )
-		f(i)
-		i = i + 1
+		f()
 		love.graphics.pop()	    
 	     end      
 	  end
@@ -92,29 +146,17 @@ function grid(cx,cy)
    })
 end
 
-
-
-function dot()
-   love.graphics.push()
-   local lineWidth = 10
-   love.graphics.setLineWidth( lineWidth )
-   love.graphics.circle( 'fill', 0, 0, 50 - lineWidth/2 )
-   love.graphics.circle( 'line', 0, 0, 50 - lineWidth/2 )   
-   love.graphics.pop()
-end
-
-function box()
+-- deprecated
+local function box()
    love.graphics.push()
    love.graphics.scale( 1 / math.sqrt(2) )
    love.graphics.rectangle('fill',-50,50,100,100)
    love.graphics.pop()	       
 end
 
-
-
-function circle(count)
+function M.circle(count)
    return Card.new(
-      {render=function(f)   
+      {render=function(self, f)   
 	  for i = 1, count do
 	     local angle = 2 * math.pi * i / count
 	     local inner = 50 / (1 + 1 / math.sin (math.pi / count) )
@@ -125,10 +167,11 @@ function circle(count)
 	     love.graphics.rotate( 2 * math.pi * i / count )
 	     love.graphics.scale( inner / 50 )
 	     love.graphics.scale( 0.9 )
-	     f(i)
+	     f()
 	     love.graphics.pop()	 
 	  end
       end
    })
 end
 
+return M
